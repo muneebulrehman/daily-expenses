d6122760005a3ef8b5f7b868ac59cf6b335fd705
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-27 19:15:13 +0530

    Added styling for forms and welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:home]
   protect_from_forgery prepend: true
   before_action :permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(*)
+    new_user_session_path
+  end
+  
+  def after_sign_in_path_for(*)
+  end
+
+  def after_sign_up_path_for(*)
+  end
 
   protected
 
