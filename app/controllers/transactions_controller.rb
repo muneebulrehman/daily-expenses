8599ee77ed5ef9d004d459f972cbc27fd0c388dc
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-30 22:02:57 +0530

    Added dropdown for selecting categories while creating transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,16 +5,18 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.new
   end
 
-  def create
+  def create()
     @transaction = Transaction.new(transaction_params)
     @transaction.user = current_user
-    @transaction.category_id = params[:category_id]
+    @transaction.category_id = transaction_params[:category_id]
     if @transaction.save
       flash[:success] = 'Transaction created'
       redirect_to user_category_path(current_user, @transaction.category)
     else
       render :new
     end
+    p "transaction params are"
+    puts 
   end
 
   def transaction_params
