13d235aafa905a675832b9530c0e7cacc0287837
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-06-30 12:37:22 +0530

    Tests for transactions

--- spec/views/transactions_page_spec.rb
+++ spec/views/transactions_page_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'Categories index page', type: :system do
+RSpec.describe 'Specific category/transaction page', type: :system do
   before do
     driven_by(:rack_test)
-    @user = User.create(name: 'admin3',
-                        email: 'admin3@email.com',
+    @user = User.create(name: 'admin4',
+                        email: 'admin4@email.com',
                         password: 'password', password_confirmation: 'password')
     @user.save
     visit 'users/sign_in'
     within('#new_user') do
-      fill_in 'Email', with: 'admin3@email.com'
+      fill_in 'Email', with: 'admin4@email.com'
       fill_in 'Password', with: 'password'
     end
     find_button('Log in').click
@@ -20,12 +20,23 @@ RSpec.describe 'Categories index page', type: :system do
     expect(page).to have_content('All your categories')
   end
 
-  it 'I can create new recipes.' do
+  it 'I can create new transactions for a category.' do
     click_link('Create new category')
     fill_in 'Name', with: 'Clothes'
     attach_file("Icon", Rails.root + "spec/fixtures/meme.jpg")
     find_button('Create Category').click
     expect(page).to have_content('Clothes')
     expect(page).to have_css("img")
+    click_link('Clothes')
+    expect(page).to have_content('Transactions')
+    expect(page).to have_content('Clothes')
+    click_link('Create new transaction')
+    fill_in 'Name', with: 'Shirt'
+    fill_in 'Amount', with: '1000'
+    find_button('Create transaction').click
+    expect(page).to have_content('Transaction created')
+    expect(page).to have_content('Shirt')
+    expect(page).to have_content('Payment: 1000.0₹')
   end
+
 end
